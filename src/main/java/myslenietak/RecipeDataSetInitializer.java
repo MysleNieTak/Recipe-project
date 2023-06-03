5d48866b61774066cd2c9b1e49a252fe17c1489e
Paulina Pionk <paulinapionk@gmail.com>
2023-06-03 16:54:31 +0200

    Swagger configuration

--- src/main/java/myslenietak/RecipeDataSetInitializer.java
+++ src/main/java/myslenietak/RecipeDataSetInitializer.java
@@ -1,12 +1,9 @@
 package myslenietak;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
-
-import static myslenietak.Complexity.EASY;
-import static myslenietak.Complexity.HARD;
 
 @Component
 public class RecipeDataSetInitializer {
@@ -19,11 +16,11 @@ public class RecipeDataSetInitializer {
 
     @PostConstruct
     void init(){
-        recipeRepository.saveAll(Arrays.asList(new Recipe("Łosoś pieczony", "Pieczony łosoś w sosie koperkowym", 120, 2, "Łosoś, śmietanka, koperek, masło, cytryna, sól, pieprz, ziemniaki, brokuł", EASY)));
+        recipeRepository.saveAll(Arrays.asList(new Recipe("Łosoś pieczony", "Pieczony łosoś w sosie koperkowym", 120, 2, "Łosoś, śmietanka, koperek, masło, cytryna, sól, pieprz, ziemniaki, brokuł", Complexity.EASY)));
         recipeRepository.saveAll(Arrays.asList(new Recipe("Pad Thai", "Klasyczny pad thai", 60, 2, "Kurczak/krewetki, makaron ryżowy, sos sojowy, sos rybny, limonka, orzechy ziemne, cukier trzcinowy", Complexity.STANDARD)));
-        recipeRepository.saveAll(Arrays.asList(new Recipe("Łosoś pieczony 1", "Pieczony łosoś w sosie koperkowym 1", 120, 2, "Łosoś, śmietanka, koperek, masło, cytryna, sól, pieprz, ziemniaki, brokuł", EASY)));
+        recipeRepository.saveAll(Arrays.asList(new Recipe("Łosoś pieczony 1", "Pieczony łosoś w sosie koperkowym 1", 120, 2, "Łosoś, śmietanka, koperek, masło, cytryna, sól, pieprz, ziemniaki, brokuł", Complexity.EASY)));
         recipeRepository.saveAll(Arrays.asList(new Recipe("Pad Thai 1", "Klasyczny pad thai 1", 60, 2, "Kurczak/krewetki, makaron ryżowy, sos sojowy, sos rybny, limonka, orzechy ziemne, cukier trzcinowy", Complexity.STANDARD)));
-        recipeRepository.saveAll(Arrays.asList(new Recipe("Łosoś pieczony 2", "Pieczony łosoś w sosie koperkowym 2", 120, 2, "Łosoś, śmietanka, koperek, masło, cytryna, sól, pieprz, ziemniaki, brokuł", EASY)));
+        recipeRepository.saveAll(Arrays.asList(new Recipe("Łosoś pieczony 2", "Pieczony łosoś w sosie koperkowym 2", 120, 2, "Łosoś, śmietanka, koperek, masło, cytryna, sól, pieprz, ziemniaki, brokuł", Complexity.EASY)));
         recipeRepository.saveAll(Arrays.asList(new Recipe("Pad Thai 2", "Klasyczny pad thai 2", 60, 2, "Kurczak/krewetki, makaron ryżowy, sos sojowy, sos rybny, limonka, orzechy ziemne, cukier trzcinowy", Complexity.STANDARD)));
 
     }
